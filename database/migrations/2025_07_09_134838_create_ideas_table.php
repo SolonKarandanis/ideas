920bba64904032c76303b9d58ffb6944b1916997
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-07-17 07:52:25 +0300

    Added user id in ideasa and comments table

--- database/migrations/2025_07_09_134838_create_ideas_table.php
+++ database/migrations/2025_07_09_134838_create_ideas_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('ideas', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('content',240);
             $table->unsignedInteger('likes')->default(0);
             $table->timestamps();
--- database/migrations/2025_07_14_052938_create_comments_table.php
+++ database/migrations/2025_07_14_052938_create_comments_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('idea_id')->constrained()->cascadeOnDelete();
             $table->string('content');
             $table->timestamps();
