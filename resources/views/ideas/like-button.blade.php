d19980b1d4e689e3b55d7589756bb0778c7e73f1
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-07-30 08:15:42 +0300

    Implementing Like functionality

--- resources/views/ideas/like-button.blade.php
+++ resources/views/ideas/like-button.blade.php
@@ -1,20 +1,22 @@
 @php use Illuminate\Support\Facades\Auth; @endphp
 <div>
-    @if(Auth::user()->hasLikedIdea($idea))
-        <form action="{{route('ideas.unlike',$idea->id)}}" method="post">
-            @csrf
-            @method('PUT')
-            <button class="fw-light nav-link fs-6">
-                <span class="fas fa-heart me-1"></span> {{$idea->likes->count()}}
-            </button>
-        </form>
-    @else
-        <form action="{{route('ideas.like',$idea->id)}}" method="post">
-            @csrf
-            @method('PUT')
-            <button class="fw-light nav-link fs-6">
-                <span class="far fa-heart me-1"></span> {{$idea->likes->count()}}
-            </button>
-        </form>
-    @endif
+    @auth()
+        @if(Auth::user()->hasLikedIdea($idea))
+            <form action="{{route('ideas.unlike',$idea->id)}}" method="post">
+                @csrf
+                @method('PUT')
+                <button class="fw-light nav-link fs-6">
+                    <span class="fas fa-heart me-1"></span> {{$idea->likes->count()}}
+                </button>
+            </form>
+        @else
+            <form action="{{route('ideas.like',$idea->id)}}" method="post">
+                @csrf
+                @method('PUT')
+                <button class="fw-light nav-link fs-6">
+                    <span class="far fa-heart me-1"></span> {{$idea->likes->count()}}
+                </button>
+            </form>
+        @endif
+    @endauth
 </div>
